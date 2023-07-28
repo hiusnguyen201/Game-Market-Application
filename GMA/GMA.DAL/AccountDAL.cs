b223c525af06b382e6cb514d2288c75eb2c95c1e
hiusnguyen201 <hiusnguyen201@gmail.com>
2023-07-28 14:09:30 +0700

    Delete all

--- GMA/GMA.DAL/AccountDAL.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GMA.DAL;
-using GMA.Models;
-
-public class AccountDAL
-{
-    private Account account = null;
-
-    private Account Get(MySqlDataReader)
-}
--- GMA/GMA.Models/Genre.cs
+++
@@ -1,9 +0,0 @@
-namespace GMA.Models;
-
-public class Genre
-{
-    public int GenreId { get; set; }
-    public string GenreName { get; set; }
-
-    public Genre() { }
-}
